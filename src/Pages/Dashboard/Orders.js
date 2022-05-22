d78fc88e92a1ce289ae8cb3636a316e22d9291a4
abedshadin <abedshadin@gmail.com>
2022-05-22 23:42:24 +0600

    order updated

--- src/Pages/Dashboard/Orders.js
+++ src/Pages/Dashboard/Orders.js
@@ -1,62 +1,69 @@
-import React, { useEffect, useState } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useParams } from "react-router-dom";
-import auth from "../../firebase.init";
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Orders = () => {
-  const [user, loading] = useAuthState(auth);
-  const [orders, setOrders] = useState([]);
-
-  const handleDelete = (id) => {
-    const proceed = window.confirm("Are you sure?");
-
-    if (proceed) {
-      const url = `http://localhost:5000/myorders/${id}`;
-      fetch(url, {
-        method: "DELETE",
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          const remaining = orders.filter((iteml) => iteml._id !== id);
-          setOrders(remaining);
-        });
+    const [user,loading] = useAuthState(auth);
+    const [orders,setOrders] = useState([]);
+    useEffect(()=>{
+        const email = user.email;
+        fetch(`http://localhost:5000/myorders?email=${email}`)
+        .then(res=>res.json())
+        .then(data=>setOrders(data));
+    },[])
+    const handleDelete = id =>{
+        const proceed = window.confirm('Are you sure?');
+       
+        
+        if(proceed){
+            const url = `http://localhost:5000/myorders/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+               
+                const remaining = orders.filter(iteml => iteml._id !== id);
+                setOrders(remaining);
+            })
+        }
     }
-  };
-  return (
-    <div className="overflow-x-auto">
-      <h2 className="text-center text-3xl">My Orders</h2>
-      <table className="table w-full">
-        <thead>
-          <tr>
-            <th></th>
-            <th>Tool Name</th>
-            <th>Quantity</th>
-            <th>Status</th>
-            <th>Action</th>
-          </tr>
-        </thead>
+    return (
+        <div class="overflow-x-auto">
+            <h2 className='text-center text-3xl'>My Orders</h2>
+        <table class="table w-full">
+         
 
-        <tbody>
-          {orders.map((order, index) => (
-            <tr key={order._id}>
-              <th>{index + 1}</th>
-              <td>{order.tool_name}</td>
-              <td>{order.quantity}</td>
-              <td className="uppercase">{order.status}</td>
-              <td className="uppercase text-danger">
-                <button
-                  className="btn btn-danger"
-                  onClick={() => handleDelete(order._id)}
-                >
-                  X
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+    <thead>
+    <tr>
+      <th></th>
+      <th>Tool Name</th>
+      <th>Quantity</th>
+      <th>Status</th>
+      <th>Action</th>
+    </tr>
+  </thead>
+ 
+         
+<tbody> 
+          
+          {
+              orders.map((order,index)=> <tr key={order._id}  >
+                <th>{index+1}</th>
+                <td>{order.tool_name}</td>
+                <td>{order.quantity}</td>
+                <td className='uppercase'>{order.status}</td>
+                <td className='uppercase text-danger'><button className='btn btn-danger' onClick={() => handleDelete(order._id)}>X</button></td>
+              </tr>)
+          }
+            
+        
+           
+           
+          </tbody>
+        </table>
+      </div>
+    );
 };
 
 export default Orders;
