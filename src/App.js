0bed4b859bb933fdbf82f57ab9b5e2eec5212df9
abedshadin <abedshadin@gmail.com>
2022-05-22 13:06:47 +0600

    private route added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Home from './Pages/Home/Home';
 import { Route, Routes } from 'react-router-dom';
 import Purchase from './Pages/Purchase/Purchase';
 import Error from './Pages/Shared/Error';
+import RequireAuth from './Pages/Login/RequireAuth';
+import Login from './Pages/Login/Login';
+
+
 
 function App() {
   return (
@@ -11,7 +15,16 @@ function App() {
       <Navbar></Navbar>
       <Routes>
       <Route path="/" element={<Home />}/>
-      <Route path="/purchase/:id" element={<Purchase />}/>
+      <Route path="/purchase/:id" element={
+    <RequireAuth>
+      <Purchase />
+      </RequireAuth>
+      }/>
+   <Route path="/login" element={<Login />}/>
+
+
+
+
       <Route path="*" element={<Error />}/>
     </Routes>
     </div>
---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            HEre
+        </div>
+    );
+};
+
+export default Login;
